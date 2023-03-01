3c5342a24376c11c7e4587d6751679ace3489827
HD <119816986+hfattor@users.noreply.github.com>
2023-03-01 14:34:54 -0800

    3/1/23

--- app.py
+++ app.py
@@ -49,8 +49,8 @@ def precipitation():
     # Query DB
     results = session.query(Measurement).with_entities(
         Measurement.date, 
-        sqlalchemy.func.sum(Measurement.prcp)
-        ).filter(Measurement.date >= '2016-08-23').group_by(Measurement.date).all()
+        Measurement.prcp
+        ).filter(Measurement.date >= '2016-08-23').all()
 
     session.close()
 
@@ -98,7 +98,7 @@ def temperature():
     results = session.query(Measurement).with_entities(
         Measurement.date, 
         Measurement.tobs
-    ).filter(Measurement.station.like('USC00519281')).filter(Measurement.date >= '2016-08-23').group_by(Measurement.date).all()
+    ).filter(Measurement.station.like('USC00519281')).filter(Measurement.date >= '2016-08-23').all()
 
     session.close()
 
@@ -119,24 +119,98 @@ def startdate(start):
     session = Session(engine)
 
     """Fetch the minimum, maximum, and average temperatures for all the dates 
-    greater than or equal to the start date supplied by the user, or a 404 if not."""
+    greater than or equal to the start date supplied by the user."""
 
-     results = session.query(Measurement).with_entities(
+    results_min = session.query(Measurement).with_entities(
         Measurement.date, 
-        Measurement.tobs
-        ).filter(Measurement.date >= {start}).group_by(Measurement.date).all()
+        func.min(Measurement.tobs)
+        ).filter(Measurement.date >= start).all()
+    
+    results_max = session.query(Measurement).with_entities(
+        Measurement.date, 
+        func.max(Measurement.tobs)
+        ).filter(Measurement.date >= start).all()
+    
+    results_avg = session.query(Measurement).with_entities(
+        Measurement.date, 
+        func.avg(Measurement.tobs)
+        ).filter(Measurement.date >= start).all()
         
     session.close()
 
-    # Create a dictionary from the row data and append to a list of temperature data
+    # Create a dictionary from the row data and append to a list of min, max, and average temperature data
     start_data = []
-    for date, tobs in results:
-        start_dict = {}
-        start_dict["date"] = date
-        start_dict["temp"] = tobs
-        start_data.append(start_dict)
+    for date, tobs in results_min:
+        min_dict = {}
+        min_dict["date"] = date
+        min_dict["min temp"] = tobs
+        start_data.append(min_dict)
+
+    for date, tobs in results_max:
+        max_dict = {}
+        max_dict["date"] = date
+        max_dict["max temp"] = tobs
+        start_data.append(max_dict)
+
+    for date, tobs in results_avg:
+        avg_dict = {}
+        avg_dict["date"] = date
+        avg_dict["avg temp"] = tobs
+        start_data.append(avg_dict)
 
     return jsonify(start_data)
+
+# JSON between start date and end date
+@app.route("/api/v1.0/<start>/<end>")
+def between(start, end):
+    # Create session (link) from Python to the DB
+    session = Session(engine)
+
+    """Fetch the minimum, maximum, and average temperatures for all the dates 
+   betwee the start and end dates supplied by the user."""
+
+    results_min = session.query(Measurement).with_entities(
+        Measurement.date, 
+        func.min(Measurement.tobs)
+        ).filter(Measurement.date >= start
+        ).filter(Measurement.date <= end).all()
+    
+    results_max = session.query(Measurement).with_entities(
+        Measurement.date, 
+        func.max(Measurement.tobs)
+        ).filter(Measurement.date >= start
+        ).filter(Measurement.date <= end).all()
+
+    
+    results_avg = session.query(Measurement).with_entities(
+        Measurement.date, 
+        func.avg(Measurement.tobs)
+        ).filter(Measurement.date >= start
+        ).filter(Measurement.date <= end).all()
+       
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of min, max, and average temperature data
+    btwn_data = []
+    for date, tobs in results_min:
+        min_dict = {}
+        min_dict["date"] = date
+        min_dict["min temp"] = tobs
+        btwn_data.append(min_dict)
+
+    for date, tobs in results_max:
+        max_dict = {}
+        max_dict["date"] = date
+        max_dict["max temp"] = tobs
+        btwn_data.append(max_dict)
+
+    for date, tobs in results_avg:
+        avg_dict = {}
+        avg_dict["date"] = date
+        avg_dict["avg temp"] = tobs
+        btwn_data.append(avg_dict)
+
+    return jsonify(btwn_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
